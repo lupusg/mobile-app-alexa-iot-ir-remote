bb8d521d974b8fb96277dac02347503aec16cc46
lupusg <just_vlad@yahoo.com>
2023-06-01 19:33:50 +0300

    Add splash screen

---
+++ src/components/screens/Splash.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import {authorize} from '../../services/ApiService';
+
+const SplashScreen = (props) => {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      await authorize();
+    };
+
+    setTimeout(() => {
+      setAppIsReady(true);
+    }, 2000);
+
+    prepare()
+        .catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    if (appIsReady) {
+      props.navigation.replace('Home');
+    }
+  }, [appIsReady, props.navigation]);
+
+  return (
+    <View style={styles.root}>
+      <Text>Loading...</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default SplashScreen;
